3a2900b16ca4f4e717a6cd4f77b7370289bb0fbc
Amit Raj <amitraj.py@gmail.com>
2020-10-10 08:10:32 +0200

    added test for butterfly problem

---
+++ test_butterfly.py
@@ -0,0 +1,29 @@
+from butterfly_edit import Butterfly
+
+
+test_data = [
+    ([[2, 1, 0, 2, 1],
+      [1, 0, 1, 0, 1],
+      [1, 0, 0, 0, 2]], -1),
+
+    ([[2, 1, 0, 2, 1],
+      [1, 0, 1, 2, 1],
+      [1, 0, 0, 2, 1]], 2),
+
+    ([[1, 1, 1, 1, 1],
+      [0, 2, 1, 1, 1]], 4),
+
+    ([[2, 1, 1],
+      [1, 1, 0],
+      [0, 1, 1]], 4),
+
+    ([[0, 1, 0],
+      [1, 2, 1],
+      [0, 1, 0]], 1)
+]
+
+
+def test_min_time():
+    for data, ans in test_data:
+        obj = Butterfly(data)
+        assert obj.get_min_time() == ans
